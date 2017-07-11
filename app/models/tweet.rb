ab946c8e4fbd94687572dc25f97f2987a61c105a
Lisa Watson <brainspecialist@users.noreply.github.com>
2017-07-11 16:59:09 -0400

    First commit

---
+++ app/models/tweet.rb
@@ -0,0 +1,11 @@
+class Tweet < ApplicationRecord
+	belongs_to :user
+
+	validates :message, presence: true
+	validates :message, length: {maximum: 140, too_long: "A tweet is only 140 max. Everybody knows that!"}
+
+	def ago(created)
+		created - Time.zone.now
+	end
+
+end
